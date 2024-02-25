0c1a505c54f9380c78658a61a8402339e2b2beea
Harshit V <100853494+harshitv804@users.noreply.github.com>
2024-02-26 01:52:14 +0530

    Update and rename VectorEmbeddings.py to Ingest.py

---
+++ Ingest.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+
+loader = DirectoryLoader('data', glob="./*.pdf", loader_cls=PyPDFLoader)
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=200)
+texts = text_splitter.split_documents(documents)
+
+embedings = HuggingFaceEmbeddings(model_name="nomic-ai/nomic-embed-text-v1",model_kwargs={"trust_remote_code":True,"revision":"289f532e14dbbbd5a04753fa58739e9ba766f3c7"})
+faiss_db = FAISS.from_documents(texts, embedings)
+faiss_db.save_local("ipc_vector_db")
