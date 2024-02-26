6dce2879d66ec01f217f90f0fa1fb400a53419be
Harshit V <100853494+harshitv804@users.noreply.github.com>
2024-02-26 11:27:33 +0530

    Update Ingest.py

--- Ingest.py
+++ Ingest.py
@@ -10,5 +10,9 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=20
 texts = text_splitter.split_documents(documents)
 
 embedings = HuggingFaceEmbeddings(model_name="nomic-ai/nomic-embed-text-v1",model_kwargs={"trust_remote_code":True,"revision":"289f532e14dbbbd5a04753fa58739e9ba766f3c7"})
+
+# Creates vector embeddings and saves it in the FAISS DB
 faiss_db = FAISS.from_documents(texts, embedings)
+
+# Saves and export the vector embeddings databse
 faiss_db.save_local("ipc_vector_db")
